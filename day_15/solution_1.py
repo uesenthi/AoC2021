11d92aecce468cea67385506da9284d33934314b
Umesh Senthilnathan <umesh@ada.support>
2021-12-15 12:24:45 -0500

    Day 15, Solution 1

---
+++ day_15/solution_1.py
@@ -0,0 +1,72 @@
+import math
+import os
+
+from libraries.helpers import get_input
+from collections import defaultdict
+
+def get_coordinate_map(content):
+    coordinate = {
+        x: []
+        for x in range(len(content))
+    }
+
+    for idx, line in enumerate(content):
+        coordinate[idx] =  list(map(lambda x: int(x), list(line)))
+
+    return coordinate
+
+def get_density_at_pos(content, y, x):
+    if x < 0 or y < 0:
+        # print("less than 0 errpr", y,x)
+        return None
+    try:
+        val = content[y][x]
+        return val
+    
+    except (KeyError,IndexError):
+        # print("index errpr", y,x)
+        return None
+
+
+def get_solution(content):
+    # STarting position is never "entered", so don't count it
+    coordinate_map = get_coordinate_map(content)
+    start_position = [0,0]
+    end_position = [len(coordinate_map[0])-1, len(coordinate_map) - 1]
+
+    shortest_path = {
+        str(start_position): 0
+    }
+
+    for y in range(len(coordinate_map)):
+        for x in range(len(coordinate_map[0])):
+            # All possible neighbors
+            possible_neighors = [
+                [y - 1, x],
+                [y, x - 1],
+                [y, x + 1],
+                [y + 1, x]
+            ]
+            actual_neighbors = list(filter(lambda x: get_density_at_pos(coordinate_map, x[0], x[1]) is not None, possible_neighors))
+
+            for neighbor in actual_neighbors:
+                density_to_neighber = get_density_at_pos(coordinate_map, neighbor[0], neighbor[1])
+                shortest_path.update({
+                    str(neighbor): min(shortest_path.get(str(neighbor),math.inf), shortest_path[str([y,x])] + density_to_neighber)
+                })
+
+    path = shortest_path[str(end_position)]
+
+    return path    
+
+
+if __name__ == "__main__":
+    test_solution = get_solution(get_input(path_to_file=os.getcwd() + "/input_test.txt"))
+    if test_solution == 40:
+        print("TEST PASSED")
+        print(get_solution(get_input(path_to_file=os.getcwd() + "/input.txt")))
+        print("622 is too high, i put 621 and it was the answer. I don' tknow why .....")
+        
+    else:
+        print(test_solution)
+        print("Test failed")
